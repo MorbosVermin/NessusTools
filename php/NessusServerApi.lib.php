520b31691518c63b36caf7d48295c12c0a46ec28
MorbosVermin <morbos.vermin@gmail.com>
2013-09-21 20:30:11 -0400

    Tested against 5.2.2 of Nessus (Debian 6; amd64) on Ubuntu 13.04. Works well to download scans. :)

--- php/NessusServerApi.lib.php
+++ php/NessusServerApi.lib.php
@@ -18,7 +18,7 @@ class NessusUser  {
   
   public function __construct($user)  {
     $this->name = $user->name;
-    $this->admin = bool($user->admin);
+    $this->admin = (bool)$user->admin;
   }
   
   public function getName()  {
@@ -81,13 +81,34 @@ class NessusReports extends ArrayList  {
   
   public function __construct($reports)  {
     parent::__construct();
-    foreach($report->report as $report)
+    foreach($reports->report as $report)
       $this->add(new NessusReport($report));
     
   }
   
 }
 
+/**
+ * Nessus Report Class
+ * 
+ * Example Reply...
+ * <pre>
+ * <reply>
+ *  <seq>7604</seq>
+ *  <status>OK</status>
+ *  <contents>
+ *    <reports>
+ *      <report>
+ *        <name>15132e77-bed3-063d-a2cd-8c9762adc0c660cd999668f6fcd7</name>
+ *        <status>running</status>
+ *        <readableName>TheArk</readableName>
+ *        <timestamp>1379801845</timestamp>
+ *     </report>
+ *    </reports>
+ *  </contents>
+ * </reply>
+ * </pre>
+ */
 class NessusReport  {
   
   private $name;
@@ -124,16 +145,41 @@ class NessusReport  {
   
 }
 
+/**
+ * Nessus Server Reply Class<p />
+ *
+ * Example logon reply...
+ * <pre>
+ * <?xml version="1.0" encoding="UTF-8"?>
+ * <reply>
+ *   <seq>3956</seq>
+ * 	 <status>OK</status>
+ * 	 <contents>
+ * 		<token>530ae8209e0fa0999a546a35b416a8a581acf867213b0d66</token>
+ * 	 	<server_uuid>e0b24d09-8226-a56f-1827-d3589dc2f624cd04046183a79a63</server_uuid>
+ * 	 	<plugin_set>201309202015</plugin_set>
+ * 	 	<loaded_plugin_set>201309202015</loaded_plugin_set>
+ * 	 	<scanner_boottime>1379801507</scanner_boottime>
+ * 	 	<msp>FALSE</msp>
+ * 	 	<idle_timeout>30</idle_timeout>
+ *   	<user>
+ * 		 <name>ncdc.its</name>
+ * 		 <admin>TRUE</admin>
+ * 	 	</user>
+ * 	 </contents>
+ * </reply>
+ * </pre>
+ */
 class NessusServerReply  {
   
   private $seq;
   private $status;
-  private $content;
+  private $contents;
   
   public function __construct($reply)  {
     $this->seq = $reply->seq;
     $this->status = $reply->status;
-    $this->content = new Content($reply->contents);
+    $this->contents = new NessusServerReplyContents($reply->contents);
   }
   
   public function getSeq()  {
@@ -144,8 +190,12 @@ class NessusServerReply  {
     return $this->status;
   }
   
-  public function getContent()  {
-    return $this->content;
+  public function getContents()  {
+    return $this->contents;
+  }
+  
+  public function getToken()  {
+    return $this->getContents()->getToken();
   }
   
   public function isOk()  {
@@ -154,7 +204,7 @@ class NessusServerReply  {
   
 }
 
-class Content  {
+class NessusServerReplyContents  {
   
   private $token;
   private $user;
@@ -178,8 +228,11 @@ class Content  {
     $this->load = $contents->load;
     $this->platform = $contents->platform;
     $this->idleTimeout = $contents->idle_timeout;
-    $this->text = $contents->toString();
-    $this->reports = new NessusReports($contents->reports);
+    $this->text = (string)$contents;
+    
+    if(isset($contents->reports))
+      $this->reports = new NessusReports($contents->reports);
+    
   }
   
   public function getToken()  {
@@ -270,7 +323,7 @@ class NessusServer  {
     $this->token = null;
   }
   
-  public function getReply($path, $params)  {
+  public function getReply($path, $params, $raw=false)  {
     
     /*
      * If we are given a string, then I will assume we have taken care of 
@@ -281,7 +334,7 @@ class NessusServer  {
       $p = $params;
       $params = "";
       foreach($p as $k => $v)
-        $params += urlencode($k) +"="+ urlencode($v) +"&";
+        $params .= urlencode($k) ."=". urlencode($v) ."&";
       
       $params = substr(0, strlen($params) - 1, $params);
     }
@@ -301,31 +354,59 @@ class NessusServer  {
         CURLOPT_POSTFIELDS     => $params,
         CURLOPT_SSL_VERIFYHOST => 0,
         CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_VERBOSE        => 1
+        CURLOPT_VERBOSE        => 0
     );
     
     //If we are logged in, a Token is needed in the request cookies.
-    if(!is_null($this->token))
-      array_push($options, array(CURLOPT_COOKIE => "token="+ $this->token));
+    if(!is_null($this->token))  {
+      $options[CURLOPT_COOKIE] = sprintf("token=%s", $this->token);
+      trigger_error(sprintf("Set Token cookie: %s", $options[CURLOPT_COOKIE]), E_USER_NOTICE);
+    }
     
     //cURL initialization, configuration, and exec call.
-    trigger_error("Connecting to "+ $this->uri + $path +"...", E_USER_NOTICE);
-    $ch      = curl_init($this->uri + $path); 
-    curl_setopt_array($ch,$options); 
+    trigger_error(sprintf("[cURL] Connecting to %s%s...", $this->uri, $path), E_USER_NOTICE);
+    $ch      = curl_init($this->uri . $path);
+    curl_setopt_array($ch,$options);
+    
+    /*
+     * Example Login Reply
+     *
+     <?xml version="1.0" encoding="UTF-8"?>
+     <reply>
+       <seq>3956</seq>
+       <status>OK</status>
+       <contents>
+         <token>530ae8209e0fa0999a546a35b416a8a581acf867213b0d66</token>
+         <server_uuid>e0b24d09-8226-a56f-1827-d3589dc2f624cd04046183a79a63</server_uuid>
+         <plugin_set>201309202015</plugin_set>
+         <loaded_plugin_set>201309202015</loaded_plugin_set>
+         <scanner_boottime>1379801507</scanner_boottime>
+         <msp>FALSE</msp>
+         <idle_timeout>30</idle_timeout>
+         <user>
+           <name>ncdc.its</name>
+           <admin>TRUE</admin>
+         </user>
+       </contents>
+     </reply>
+     */
     $content = curl_exec($ch); 
     
     $err     = curl_errno($ch); 
     $errmsg  = curl_error($ch); 
     $header  = curl_getinfo($ch); 
-    trigger_error("Received HTTP code "+ $header["http_code"] +" from "+ $this->uri + $path +" ("+ strlen($content) +"bytes)", E_USER_NOTICE);
-    trigger_error($content, E_USER_NOTICE);
+    trigger_error(sprintf("[cURL] Received HTTP code %d from %s%s (%db)", $header["http_code"], $this->uri, $path, strlen($content)), E_USER_NOTICE);
+    trigger_error(str_replace("\n", "\n [cURL] ", "[cURL] ". $content), E_USER_NOTICE);
     
     curl_close($ch);
     
     if($err > 0)
       throw new NessusServerException($header, $errno, $errmsg);
     
-    return new NessusServerReply(simplexml_load_string($content));
+    if($header["http_code"] != 200)
+      throw new NessusServerException($header, $header["http_code"], "Did not receive HTTP OK (200) from server.");
+    
+    return ($raw) ? $content : new NessusServerReply(simplexml_load_string($content));
   }
   
   private function r()  {
@@ -340,10 +421,11 @@ class NessusServer  {
     try  {
       $reply = $this->getReply("/login", $params);
       $this->token = $reply->getToken();
+      trigger_error(sprintf("Logged on successfully as user: %s", $username), E_USER_NOTICE);
       return true;
     
     }catch(NessusServerException $e)  {
-      trigger_error("Could not logon to server "+ $this->uri +": "+ $e->getMessage(), E_USER_ERROR);
+      trigger_error(sprintf("Could not logon to server %s: %s", $this->uri, $e->getMessage()), E_USER_ERROR);
     }
     
     return false;
@@ -354,35 +436,56 @@ class NessusServer  {
       $this->getReply("/logout", sprintf("seq=%d", $this->r()));
     
     }catch(NessusServerException $e)  {
-      trigger_error("Could not logout of server "+ $this->uri +": "+ $e->getMessage(), E_USER_WARNING);
+      trigger_error(sprintf("Could not logout of server %s: %s", $this->uri, $e->getMessage()), E_USER_WARNING);
     
     }
   }
   
+  /**
+   * Returns a NessusReports object for the reports on the server. You will 
+   * need to login prior to this call which will set the appropriate token.
+   *
+   * @return	object NessusReports
+   */
   public function getReports()  {
     $reply = null;
     try  {
       $reply = $this->getReply("/report/list", sprintf("seq=%d", $this->r()));
     
     }catch(NessusServerException $e)  {
-      trigger_error("Could not get reports from "+ $this->uri +": "+ $e->getMessage(), E_USER_ERROR);
+      trigger_error(sprintf("Could not get list of reports from %s: %s", $this->uri, $e->getMessage()), E_USER_WARNING);
+      
     }
     
-    return (!is_null($reply)) ? $reply->getContent()->getReports() : null;
+    return (!is_null($reply)) ? $reply->getContents()->getReports() : null;
   }
   
+  /**
+   * Will download a report by the given UUID and store the report at given $path.
+   * This will then return the report as well. 
+   *
+   * @param	string	UUID of the report to download.
+   * @param	string	Path to store the report file.
+   * @return object NessusReport
+   */
   public function downloadReport($uuid, $path)  {
-    $report = null;
+    $scan = null;
     try  {
-      $scan_xml = $this->getReply("/file/report/download", sprintf("seq=%d&report=%s", $this->r(), $uuid));
-      $report = new NessusReport(simplexml_load_string($scan_xml));
+      $scan_xml = $this->getReply("/file/report/download", sprintf("seq=%d&report=%s", $this->r(), $uuid), true);
+      file_put_contents($path, $scan_xml);
+      trigger_error(sprintf("Downloaded %dbytes to %s", filesize($path), $path), E_USER_NOTICE); 
+      
+      $scan = NessusClientData::parse($path);
     
     }catch(NessusServerException $e)  {
-      trigger_error("Could not download report from "+ $this->uri +": "+ $e->getMessage(), E_USER_ERROR);
+      trigger_error(sprintf("Could not download report '%s' from %s: %s", $uuid, $this->uri, $e->getMessage()), E_USER_WARNING);    
     
+    }catch(FileNotFoundException $e)  {
+      trigger_error(sprintf("Could not open/read %s. Perhaps the XML did not save properly.", $path), E_USER_ERROR);
+      
     }
     
-    return $report;
+    return $scan;
   }
   
 }
